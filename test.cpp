275d43481c74d37e7758fd02765b9d5c8ace4c11
anxiao <anxiao@localhost.localdomain>
2018-07-20 09:30:17 +0800

    for test.

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+/*union node
+{
+	unsigned int m;
+	char c;
+};
+*/
+int main(void)
+{
+	int x=100,a;
+	int y=100,b;
+	a=++x;
+	b=y++;
+
+
+	cout<<a<<endl<<b<<endl;
+	return 0;
+	int c=a+b;
+	cout<<c<<endl;
+        cout<<"你好"<<endl;
+	return 0;
+}
